cc13df84d8c8baa6334e36c7c7d87dc3509d37d2
Dylen <dylenbelanger2004@gmail.com>
2023-05-05 14:47:46 -0400

    Create functions to add/sub/multiply/divide

---
+++ script.js
@@ -0,0 +1,20 @@
+// add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// divide two numbers
+function divide(a, b) {
+    return a / b;
+}
+
