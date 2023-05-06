aebc97918cea64d7538c1f84b7f7d26274ca6900
Dylen <dylenbelanger2004@gmail.com>
2023-05-05 23:53:39 -0400

    Fixed multiple bugs, including being unable to perform multiple operations before calculating the result

--- script.js
+++ script.js
@@ -46,8 +46,6 @@ function clearDisplay() {
 function resetVariables() {
     number = "";
     operator = "";
-    result = null
-    operatorChosen = false;
     digits = [];
     numbers = [];
 }
@@ -58,8 +56,6 @@ let operator = "";
 let result = null;
 let digits = []; // digits for number on display
 let numbers = []; // array to hold numbers
-let operatorChosen = false; // key to decide if an operator has been chosen
-let temp = null; // used to store result for later calculations
 
 // create element to store display item
 const display = document.querySelector(".display");
@@ -76,12 +72,10 @@ buttons.forEach(button => {
 
         // runs if buttonContent is a number
         if (!isNaN(buttonContent)) {
-            temp = null; // temp variable must be set to null if a new calculation is happening
 
-            // clears digits if operator is chosen
-            if (operatorChosen) {
-                digits = [];
-                operatorChosen = false;
+            // We need to clear numbers array if user enters a number immediately after a calculation 
+            if (!["+","-","*","/"].includes(operator)) {
+                numbers = [];
             }
 
             digits.push(buttonContent)
@@ -92,51 +86,53 @@ buttons.forEach(button => {
         // runs if clear button is clicked (resets all variables)
         else if (buttonContent == "AC") {
             clearDisplay();
-            temp = null;
+            result = null;
             resetVariables();
         }
 
-        // runs if equals button is clicked
-        else if (buttonContent == "=") {
-
-            // add current number to array
-            numbers.push(parseInt(number));
-
-            // if only one number chosen, set second number to 0
-            if (numbers.length == 1) {
-                numbers.push(0);
-            }
-
-            console.log(numbers) //TEST
-
-            // check if two numbers are chosen
-            if (numbers.length == 2) {
-                clearDisplay();
-                result = operate(operator, numbers[0], numbers[1]);
-                console.log(result) //TEST
-
-                displayItem.textContent = result; // sets text content of display to be result
-                display.appendChild(displayItem);
-                temp = result // temp variable to hold result
-                resetVariables();
-            }
-        }
-
-        // runs if an operator is clicked
+        // runs if equals or operator button is clicked
         else {
-            operator = buttonContent;
-            operatorChosen = true;
 
-            if (temp != null) {
-                numbers.push(temp);
-            } else if (digits.length == 0) {
-                numbers.push(0); // add 0 if an operator is clicked first
-            } else {
-                numbers.push(parseInt(number)); // adds current number to array
-                digits = [];
+            if (buttonContent == "=") {
+
+                // add current number to array
+                numbers.push(parseInt(number));
+
+                // if only one number chosen, set second number to 0
+                if (numbers.length == 1) {
+                    numbers.push(0);
+                }
+                console.log(numbers)
             }
 
-            console.log(numbers) //TEST
+            // runs if an operator is clicked
+            else {
+                if (digits.length == 0) {
+                    numbers.push(0); // add 0 if an operator is clicked first
+                    console.log(numbers)
+                }
+
+                // else just set the operator
+
+                else {
+                    numbers.push(parseInt(number));
+                    digits = [];
+                    console.log(numbers)
+                }
+            }
+
+            // perform calculation on two numbers
+            if (numbers.length == 2) {
+                result = operate(operator, numbers[0], numbers[1])
+                displayItem.textContent = result;
+                resetVariables();
+                numbers[0] = result;
+                console.log(result)
+                console.log(numbers)
+                result = null;
+            }
+
+            operator = buttonContent;
         }
     })
 })
@@ -144,6 +140,4 @@ buttons.forEach(button => {
 // BUGS
 
 
-// 2. after calculating a result, set the value as the first number to the next operation
-// 3. allow multiple operations to be performed at once before calculating result
 // 4. clicking equals multiple times leads to NaN
